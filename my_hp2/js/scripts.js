9bd92ae5e6c4ad8136b5736efdfc725a81a86591
t.miyata <makestein.m@gmail.com>
2024-07-18 04:12:02 +0900

    delete gray

--- my_hp2/js/scripts.js
+++ my_hp2/js/scripts.js
@@ -1,7 +1,5 @@
 /*!
-* Start Bootstrap - Grayscale v7.0.6 (https://startbootstrap.com/theme/grayscale)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+* Copyright &copy; 2024 E-INS INC. All rights Reserved.
 */
 //
 // Scripts
